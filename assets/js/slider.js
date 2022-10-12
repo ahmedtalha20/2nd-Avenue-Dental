eda3937807a55d43dcbcf2c0803be65c593d6599
Talha Prodhan <ahmedtalha20@gmail.com>
2022-10-12 12:44:10 -0600

    fixed bug in slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -18,9 +18,18 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
+
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
+
+  if(slides[slideIndex-1] != undefined){
+    slides[slideIndex-1].style.display = "block";  
+  }
+
+  if(dots[slideIndex-1] != undefined){
+    dots[slideIndex-1].className += " active";
+  }
+
+  
 }
